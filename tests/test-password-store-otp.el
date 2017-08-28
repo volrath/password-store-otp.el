1f11d27bed285f4acbcd8287afb929379301c073
Daniel Barreto <dbarreto@talpor.com>
2017-08-28 13:51:06 +0200

    Add a mock test

---
+++ tests/test-password-store-otp.el
@@ -0,0 +1,58 @@
+;;; test-password-store-otp.el --- 
+;; 
+;; Filename: test-password-store-otp.el
+;; Description: 
+;; Author: Daniel Barreto
+;; Maintainer: 
+;; Created: Mon Aug 28 13:35:31 2017 (+0200)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: 
+;;           By: 
+;;     Update #: 0
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+(require 'buttercup)
+(require 'password-store-otp)
+
+(describe "first test"
+          (it "passes"
+              (expect (= 1 1)
+                      :to-equal
+                      t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; test-password-store-otp.el ends here
